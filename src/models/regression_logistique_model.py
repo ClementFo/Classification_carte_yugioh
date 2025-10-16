860c0758dc74d9a17ddb25e56cbf038ea105dcce
clement <clementfornes@gmail.com>
2025-10-16 11:37:32 +0200

    Modification de la gestion de l'entrainement et de la prediction

--- src/models/regression_logistique_model.py
+++ src/models/regression_logistique_model.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import logging
 from typing import Any, Dict, cast
 
@@ -18,6 +19,13 @@ import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 warnings.filterwarnings("ignore", ".*iCCP.*")
 logging.getLogger("PIL.PngImagePlugin").setLevel(logging.ERROR)
+
+
+# Analyse des arguments en ligne de commande
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--train', action='store_true', help='Entrainement')
+parser.add_argument('-e', '--eval', action='store_true', help='Evaluation')
+args = parser.parse_args()
 
 filtres = {
     "Sans": "data/Yugi_images",
@@ -66,59 +74,76 @@ for nom_filtre, path in filtres.items():
             solver=solver,
         )
 
-        # Entrainement du modèle de régréssion logistique
-        clf.fit(X_train, y_train)
+        if args.train:
+            print("==>==>Entrainement Model")
+            # Entrainement du modèle de régréssion logistique
+            clf.fit(X_train, y_train)
 
-        # Prédiction du modèle de régréssion logistique
-        y_pred = clf.predict(X_test)
-        y_proba = clf.predict_proba(X_test)
+            # Enregistrement du modèle de régréssion logistique
+            modele_name_file = "models/" + model_type + "_" + penalty + "_" + solver
+            modele_name_file = modele_name_file + "_" + nom_filtre
+            with open(modele_name_file, 'wb') as fichier:
+                pickle.dump(clf, fichier)
 
-        # Enregistrement du modèle de régréssion logistique
-        modele_name_file = "models/" + model_type + "_" + penalty + "_" + solver + "_"
-        modele_name_file = modele_name_file + "_" + nom_filtre
-        with open(modele_name_file, 'wb') as fichier:
-            pickle.dump(clf, fichier)
+        if args.eval:
+            print("==>==>Evaluation Model")
+            with open(f'models/regression_logistique_{penalty}_{solver}_{nom_filtre}',
+                      'rb') as fichier:
+                clf = pickle.load(fichier)
 
-        # Génération de la matrice de confusion et des métriques
-        cm = confusion_matrix(y_test, y_pred)
-        raw_report: Any = classification_report(
-            y_test, y_pred, zero_division=0, output_dict=True
-        )
-        report: Dict[str, Any] = cast(Dict[str, Any], raw_report)
-        
-        if y_proba.shape[1] == 2:
-            # Cas binaire
-            y_proba_pos = y_proba[:, 1]
-            roc_auc = roc_auc_score(y_test, y_proba_pos)
-        else:
-            # Cas multi-classes
-            y_test_bin = label_binarize(y_test, classes=np.unique(y_test))
-            roc_auc = roc_auc_score(y_test_bin, y_proba, multi_class='ovr')
-        results: Dict[str, float | str] = {
-            "C": C,
-            "max_iter": max_iter,
-            "penalty": penalty,
-            "solver": solver,
-            "nom_filtre": nom_filtre,
-            "roc_auc": float(roc_auc),
-        }
-        results.update(
-            {f"cm_{i}_{j}": cm[i, j] for i in range(
-                cm.shape[0]) for j in range(cm.shape[1])})
+            # Prédiction du modèle de régréssion logistique
+            y_pred = clf.predict(X_test)
+            y_proba = clf.predict_proba(X_test)
 
-        for label, metrics in report.items():
-            if isinstance(metrics, dict):
-                metrics_dict: Dict[str, float] = metrics
-                results[f"{label}_precision"] = float(
-                    metrics_dict.get("precision", 0.0))
-                results[f"{label}_recall"] = float(
-                    metrics_dict.get("recall", 0.0))
-                results[f"{label}_f1"] = float(
-                    metrics_dict.get("f1-score", 0.0))
+            # Génération de la matrice de confusion et des métriques
+            cm = confusion_matrix(y_test, y_pred)
+            table = pd.crosstab(y_test, y_pred, rownames=['Classe réelle'],
+                                colnames=['Classe prédite'])
+            plt.figure(figsize=(6, 4))
+            sns.heatmap(table, annot=True, fmt="d", cmap="Blues")
+            plt.title("Matrice de confusion")
+            dir = "reports/figures/matrice_confusion_regression_logistique"
+            plt.savefig(f"{dir}_{penalty}_{solver}_{nom_filtre}.png")
+            plt.show()
 
-        # Ajout des métriques dans le dataframe df_results
-        df_results = pd.concat([df_results,
-                                pd.DataFrame([results])], ignore_index=True)
+            raw_report: Any = classification_report(
+                y_test, y_pred, zero_division=0, output_dict=True
+            )
+            report: Dict[str, Any] = cast(Dict[str, Any], raw_report)
+            
+            if y_proba.shape[1] == 2:
+                # Cas binaire
+                y_proba_pos = y_proba[:, 1]
+                roc_auc = roc_auc_score(y_test, y_proba_pos)
+            else:
+                # Cas multi-classes
+                y_test_bin = label_binarize(y_test, classes=np.unique(y_test))
+                roc_auc = roc_auc_score(y_test_bin, y_proba, multi_class='ovr')
+            results: Dict[str, float | str] = {
+                "C": C,
+                "max_iter": max_iter,
+                "penalty": penalty,
+                "solver": solver,
+                "nom_filtre": nom_filtre,
+                "roc_auc": float(roc_auc),
+            }
+            results.update(
+                {f"cm_{i}_{j}": cm[i, j] for i in range(
+                    cm.shape[0]) for j in range(cm.shape[1])})
+
+            for label, metrics in report.items():
+                if isinstance(metrics, dict):
+                    metrics_dict: Dict[str, float] = metrics
+                    results[f"{label}_precision"] = float(
+                        metrics_dict.get("precision", 0.0))
+                    results[f"{label}_recall"] = float(
+                        metrics_dict.get("recall", 0.0))
+                    results[f"{label}_f1"] = float(
+                        metrics_dict.get("f1-score", 0.0))
+
+            # Ajout des métriques dans le dataframe df_results
+            df_results = pd.concat([df_results,
+                                    pd.DataFrame([results])], ignore_index=True)
 
 
 # Génération d'un histogramme du weighted avg_f1
@@ -127,7 +152,6 @@ sns.barplot(
     data=df_results,
     x="nom_filtre",
     y="weighted avg_f1",
-    hue="category",
     palette="tab10"
 )
 plt.title("Meilleur weighted avg_f1 par Filtre et Catégorie", pad=15)
@@ -139,14 +163,13 @@ plt.tight_layout()
 plt.savefig("reports/figures/result_regression_logistique.png")
 plt.show()
 
-
 # Sauvegarde dans CSV
 data_dir = os.path.join("data", "Result")
-output_file = os.path.join(data_dir, "results_regression_logistique_1.csv")
+output_file = os.path.join(data_dir, "results_regression_logistique.csv")
 
-if os.path.exists("reports/csv_export/results_regression_logistique_1.csv"):
-    df_results.to_csv("reports/csv_export/results_regression_logistique_1.csv",
+if os.path.exists("reports/csv_export/results_regression_logistique.csv"):
+    df_results.to_csv("reports/csv_export/results_regression_logistique.csv",
                       mode="a", index=False, sep=";")
 else:
-    df_results.to_csv("reports/csv_export/results_regression_logistique_1.csv",
+    df_results.to_csv("reports/csv_export/results_regression_logistique.csv",
                       index=False, sep=";")
