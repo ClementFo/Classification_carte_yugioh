5ae0c6a3e95e992bd21caf005608a7f58322f45d
clement <clementfornes@gmail.com>
2025-10-17 16:07:38 +0200

    Mise en place de django

---
+++ Classification_carte_yugioh/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Classification_carte_yugioh project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Classification_carte_yugioh.settings")
+
+application = get_wsgi_application()
